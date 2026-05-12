335e6446ff31d94c964f1e9dae3c0c3aa5d7800b
Eneko <enekoisasiugarte@gmail.com>
2026-05-12 23:14:25 +0200

    Cambios 1205 2

--- calculo.py
+++
@@ -1,6 +0,0 @@
-precio_leche = 1.20
-cantidad_cajas = 3
-total_compra = precio_leche * cantidad_cajas
-
-print(f"Has comprado {cantidad_cajas} cajas.")
-print(f"El precio total es: {total_compra}")
--- experimento.py
+++
@@ -1,4 +0,0 @@
-precio_producto = 100
-descuento = 20
-total = precio_producto - descuento
-print(f"El precio final es {total}")
